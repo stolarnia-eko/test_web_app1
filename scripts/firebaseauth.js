8b998c578127f766b6946397fb1fe48d0684c17c
stolarnia-eko <141035353+stolarnia-eko@users.noreply.github.com>
2025-12-10 13:10:00 +0100

    Implement Google authentication with popup
    Added Google sign-in functionality using signInWithPopup.

--- scripts/firebaseauth.js
+++ scripts/firebaseauth.js
@@ -32,6 +32,29 @@ onAuthStateChanged(auth, (user) => {
         console.log(auth.currentUser)
     }
 });
+const provider = new GoogleAuthProvider();
+document.getElementById('google').addEventListener('click', e => {
+    console.log('google')
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+})
 
 
 document.getElementById('a').addEventListener('click', e => {
